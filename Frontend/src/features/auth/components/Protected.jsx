ccc3eac4a8b77d73a78b55df65d481bdff1a6137
harshsingh1678 <singhharsh88319@gmail.com>
2026-07-21 20:43:01 +0530

    update Protected component

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -1,16 +1,15 @@
-import { useNavigate } from "react-router"
+import { Navigate } from "react-router"
 import { useAuth } from "../hooks/useAuth"
 
 const Protected = ({ children }) => {
     const { loading, user } = useAuth()
-    const navigate = useNavigate()
 
     if (loading) {
         return (<main><h1>Loading...</h1></main>)
     }
 
     if (!user) {
-        navigate('/login')
+        return <Navigate to={'/login'} />
     }
 
     return children
